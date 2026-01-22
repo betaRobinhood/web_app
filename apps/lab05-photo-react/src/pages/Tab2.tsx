f364d2d4ecc727f4a30a7600d45fc0977f8d0eaa
koeproject <chnwirprajan@gmail.com>
2026-01-22 16:29:27 +0700

    deploy lab05 take photo with react

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,58 @@
+import { camera } from "ionicons/icons";
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from "@ionic/react";
+import "./Tab2.css";
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">ชนวีร์ ประจันทร์ รหัส 663380010-0</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">ชนวีร์ ประจันทร์ รหัส 663380010-0</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src="{photo.webviewpath}" />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
